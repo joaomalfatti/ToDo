f49140633d95e4845ddbc7b8d682460d95e49cb9
joaomalfatti <joao.cursos1210@gmail.com>
2024-07-28 00:52:51 -0400

    Finalizado o desafio, adicionamos o botão e exclusão por final

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,16 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 
 /* Vamos trazer os estilos do mobile*/
 import { styles } from "./styles";
-import { Text, View, Image, TextInput, TouchableOpacity} from "react-native";
+import { Text, View, Image, TextInput, TouchableOpacity, Alert, FlatList} from "react-native";
 
 import rocket from '../../assets/rocket.png';
 import Clipboard from '../../assets/Clipboard.png';
+import { CompEvent } from "../../CompEvent";
 
 
 
 export default function Home(){
+
+  
+  const [events, setEvents] = useState<string[]>([]);
+
+  const [eventName, setEventName] = useState('');
+
+  function handleEventAdd(){
+    /* Verificação caso o evento existe, se existir ele não adiciona*/
+    if(events.includes(eventName)){
+      return Alert.alert("Evento existe", "Já existe um evento com esse nome.")
+    }
+
+    /*Depois quando passar da verificação, vai para adicionar o evento*/
+    setEvents(prevState => [...prevState, eventName]);
+    setEventName('');
+  }
+
+  function handleEventRemove (name: string){
+
+    Alert.alert('Remover', `Remover o evento ${name}?`, [
+      {
+        text:'Sim',
+        /*Deletar o evento paraque ele some do array*/
+        onPress:() => setEvents(prevState => prevState.filter(CompEvent => CompEvent != name))
+      },
+      {
+        text: 'Não',
+        style: 'cancel'
+      }
+    ])
+  }
+
+
   return(
+
     <View style={styles.oneContainer}>
       {/* Primeira view sobre foguete e o*/}
       <View style={styles.oneView}>
@@ -34,11 +69,15 @@ export default function Home(){
           style={styles.input}
           placeholder="Adicone uma nova tarefa"
           placeholderTextColor="#808080"
+          onChangeText={setEventName}
+          value={eventName}
         />
-        <TouchableOpacity style={styles.addButton}>
-          <Text style={styles.addButtonText}>
-            +
-          </Text>
+        <TouchableOpacity 
+          style={styles.addButton}
+          onPress={handleEventAdd}>
+            <Text style={styles.addButtonText}>
+              +
+            </Text>
         </TouchableOpacity>
 
       </View>
@@ -78,7 +117,18 @@ export default function Home(){
           Crie tarefas e organize seus itens a fazer
         </Text>
       </View>
-      
+
+      <FlatList
+        data={events}
+        keyExtractor={item => item}
+        renderItem={({item}) => (
+          <CompEvent
+          key = {item}
+          name = {item}
+          onRemove={() => handleEventRemove(item)}
+          />
+        )}
+      />
     </View>
   );
 }
